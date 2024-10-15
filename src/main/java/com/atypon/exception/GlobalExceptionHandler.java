70eb3edd6947b2289265f5197ed7f2088cae699c
Ahmad alsanie <ahmad.alsanie@hotmail.com>
2024-10-15 16:41:56 +0300

    Feat: error handling

---
+++ src/main/java/com/atypon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.atypon.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(Model model) {
+        model.addAttribute("message", "Oops! This page doesn't exist. Visit home page to view the endpoints");
+        return "error";
+    }
+}
+
